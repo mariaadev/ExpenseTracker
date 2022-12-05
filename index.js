0fa2dda12645760e977c3fdfc336b55bc1a0d508
Maria Amzil <mini.mar@hotmail.com>
2022-12-05 11:25:49 +0100

    Add functionality Edit Button Cancel Button on Edit Menu

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
 let selectConcept = document.getElementById("select-concept");
 let selectText = document.getElementById("select-text");
 let options = document.getElementsByClassName("options");
-options = Array.from(options)
-let arrow = document.getElementsByClassName("arrow")
+options = Array.from(options);
 
-let conceptList = document.getElementById("concept-list")
+let conceptList = document.getElementById("concept-list");
 
 selectConcept.addEventListener("click", (event) => {
-    conceptList.classList.toggle("hide")
+    conceptList.classList.toggle("hide");
 })
 
 options.forEach(option => {
@@ -17,8 +16,23 @@ options.forEach(option => {
         //get emoji and text from selectedOption
         selectConcept.style.display = "flex"
         selectConcept.style.justifyContent = "flex-start"
-        selectConcept.innerHTML = option.innerHTML
-        conceptList.classList.toggle("hide")
+        selectConcept.innerHTML = option.innerHTML;
+        conceptList.classList.toggle("hide");
     })
 });
 
+let editMenu = document.querySelector(".edit-menu-wrapper");
+let meatballButton = document.querySelector(".meatball-menu");
+meatballButton.addEventListener("click", (event) => {
+    editMenu.classList.toggle("active")
+})
+
+let editButton = document.querySelector(".edit")
+let cancelButton = document.querySelector(".cancel")
+
+cancelButton.addEventListener("click", (event) => {
+    editMenu.classList.toggle("active")
+})
+
+editButton.addEventListener("click", (event) => {
+})
