560d7fe2b6feb3443f0151542576883e5d6ec5fb
Maria Amzil <mini.mar@hotmail.com>
2022-12-05 17:23:21 +0100

    Add items when user adds transactions WIP

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let options = document.getElementsByClassName("options");
 options = Array.from(options);
 
 let conceptList = document.getElementById("concept-list");
+let isConceptSelected = false
 
 selectConcept.addEventListener("click", (event) => {
     conceptList.classList.toggle("hide");
@@ -18,6 +19,7 @@ options.forEach(option => {
         selectConcept.style.justifyContent = "flex-start"
         selectConcept.innerHTML = option.innerHTML;
         conceptList.classList.toggle("hide");
+        isConceptSelected = true
     })
 });
 
@@ -38,36 +40,76 @@ editButton.addEventListener("click", (event) => {
 })
 
 
-
+//button
 let addTransaction = document.querySelector(".submit-button")
+
+addTransaction.addEventListener("click", (event) => {
+    // check input is a number, if it is positive or negative and sum totals
+    event.preventDefault()
+   //input values
 let inputText = document.getElementById("amount")
+let inputNumber = parseFloat(inputText.value)
+//before UI income expenses
 let incomeUI = document.querySelector(".income-amount")
 let expensesUI = document.querySelector(".expenses-amount")
 let savings = document.querySelector(".amount-savings")
+//after UI income expenses
+let incomeTotal = parseFloat(incomeUI.innerText.replace("€",""))
+let expensesTotal = parseFloat(expensesUI.innerText.replace("€",""))
+let savingsTotal = parseFloat(savings.innerText.replace("€",""))
 
-addTransaction.addEventListener("click", (event) => {
-    // comprobar que input es 1 número
-    //comprobar si es positivo o negativo
-    //sumarlo a Income o Expenses dependiendo de neg o posi
-    event.preventDefault()
-    let inputNumber = parseFloat(inputText.value)
-    let incomeTotal = parseFloat(incomeUI.innerText.replace("€",""))
-    let expensesTotal = parseFloat(expensesUI.innerText.replace("€",""))
-    let savingsTotal = parseFloat(savings.innerText.replace("€",""))
-    if (inputNumber) {
+    if (inputNumber && isConceptSelected) {
         if (inputNumber > 0) {
             incomeTotal = inputNumber + incomeTotal
             incomeUI.innerText = `${parseFloat(incomeTotal).toFixed(2)}€`
             savings.innerText = `${parseFloat(incomeTotal - expensesTotal).toFixed(2)}€`
+            addItem()
         }
         else {
             expensesTotal = Math.abs(inputNumber - expensesTotal)
             expensesUI.innerText = `${parseFloat(expensesTotal).toFixed(2)}€`
             savings.innerText = `${parseFloat(incomeTotal - expensesTotal).toFixed(2)}€`
+            addItem()
         }
     }
   
 })
 
+let cardPlaceholder = document.querySelector(".card-placeholder")
+let cardText = document.querySelector(".card-text")
+let cardEmojisPlaceholder = document.querySelector(".emojis-placeholder")
 
 
+
+function addItem() {
+  if (isConceptSelected) {
+    cardText.remove()
+    cardEmojisPlaceholder.remove()
+    let itemContainer = document.createElement("div")
+    itemContainer.style.backgroundColor = "#6D6D6D8A"
+    itemContainer.style.width = "324px"
+    itemContainer.style.height = "50px"
+    itemContainer.style.borderRadius = "15px"
+    itemContainer.style.boxShadow = "0px 4px 4px 0px #00000040" 
+    itemContainer.style.display = "flex"
+    itemContainer.style.justifyContent = "flex-start"
+    itemContainer.style.alignItems = "center"
+    itemContainer.style.padding = "7px"
+    let emojiWrapper = document.createElement("div")
+    emojiWrapper.style.display = "flex"
+    emojiWrapper.style.justifyContent = "center"
+    emojiWrapper.style.alignItems = "center"
+    emojiWrapper.style.width = "55px"
+    emojiWrapper.style.height = "43px"
+    emojiWrapper.style.backgroundColor = " #6D6D6D8A"
+    emojiWrapper.style.boxShadow = "0px 4px 4px 0px #00000040" 
+    emojiWrapper.style.borderRadius = "15px"
+    let emoji = selectConcept.querySelector(".emoji")
+    emojiWrapper.innerText = emoji.innerText
+    emojiWrapper.style.fontSize = "2.3rem"
+    emojiWrapper.style.textAlign = "center"
+    itemContainer.appendChild(emojiWrapper)
+    cardPlaceholder.appendChild(itemContainer)
+  }
+    
+}
